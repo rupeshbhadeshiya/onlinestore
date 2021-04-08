0d0fa8b3a91dd99ebdfb273d34a2e0b741c601b9
Rupesh Bhadeshiya <rupesh.bhadeshiya@gmail.com>
2021-04-08 23:17:31 +0530

    Rectified Maven build errors for all Projects

---
+++ onlinestore-inventory-service/src/com/learning/ddd/onlinestore/inventory/boot/InventoryServiceSpringBootApp.java
@@ -0,0 +1,23 @@
+package com.learning.ddd.onlinestore.inventory.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan( {"com.learning.ddd.onlinestore.*"} )
+public class InventoryServiceSpringBootApp extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(InventoryServiceSpringBootApp.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceSpringBootApp.class, args);
+	}
+
+}
+

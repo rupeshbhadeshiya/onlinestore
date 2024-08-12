a89fe688b28bb7d3f87d7431a04f607330faaa18
Rupesh Bhadeshiya <rupesh.bhadeshiya@gmail.com>
2024-08-13 00:13:54 +0530

    Simple Test junit class for APIGW service

---
+++ apigw-service/src/test/java/com/learning/ddd/onlinestore/apigw/APIGWServiceBootAppTest.java
@@ -0,0 +1,14 @@
+package com.learning.ddd.onlinestore.apigw;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class APIGWServiceBootAppTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
